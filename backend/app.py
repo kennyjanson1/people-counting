69651565b4aaa13931ce1192bde8f70c7408402e
kennyjanson1 <kennyjanson1@gmail.com>
2025-12-06 12:18:58 +0700

    fixing port for deploy

--- backend/app.py
+++ backend/app.py
@@ -420,4 +420,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
